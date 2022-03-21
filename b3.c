61675c5ad21f7f42054708b665f3f97ba07399dc
thinh909thinh <51990581+thinh909thinh@users.noreply.github.com>
2022-03-21 20:46:57 +0700

    Add files via upload

--- b3.c
+++ b3.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 int main(){
-    int a,i=0 ,j=0;
+    int a,i ,j;
     printf("nha vao so dong a:");
     scanf("%d",&a);
+    i=0;
     while (i<=a)
     {
+        j=0;
         while (j<=i) { 
                printf("*");
                 j++;   
