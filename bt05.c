f8cd2115d79dab9f520a1881237cd0ce0f26fdb7
thinh909thinh <51990581+thinh909thinh@users.noreply.github.com>
2022-03-22 09:41:05 +0700

    Add files via upload

--- bt05.c
+++ bt05.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
-int main(){
-    int r=0,n;
-    printf("nhap n");
-    scanf("%d",&n);
-    while ( n>=0&&n<10)
+int  main () {
+    int r = 0 , n;
+    printf ( " nhap n " );
+    scanf ("%d",&n);
+    while(n>=0 && n<10)
     {
-      n=n+r;
-      r++;
+        printf ( " nhap n " );
+        scanf ("%d",&n);
+        r ++;
     }
-    printf("r bang:%d",r);
+    printf ( " r bang: % d " , r);
 
 
 
